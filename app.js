f64c993c2671b78f6deb835c82221d633cad4ddd
Soumadiptya Saha <soumadiptyas23@gmail.com>
2020-06-15 13:45:18 +0530

    Bonus done except handling ",," case

--- app.js
+++ app.js
@@ -7,14 +7,21 @@
         $scope.items = "";
         $scope.message = "";
 
-        $scope.check = function () {
+        $scope.check = function () {            
             var item = $scope.items.split(',');
+
             if ($scope.items == "") {
+                document.getElementById("msg").style.color = "red";
+                document.getElementById("lunch-menu").style.borderColor = "red";
                 $scope.message = "Please enter data first";
             } else if (item.length <= 3) {
                 $scope.message = "Enjoy!";
+                document.getElementById("msg").style.color = "green";
+                document.getElementById("lunch-menu").style.borderColor = "green";
             } else {
                 $scope.message = "Too Much!";
+                document.getElementById("msg").style.color = "green";
+                document.getElementById("lunch-menu").style.borderColor = "green";
             }
         };
     };
